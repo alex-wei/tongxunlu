920a2bd86be104643dfc89829b224a6713a392f2
Alex_Wei <Alex_Wei@cn.dlink.com>
2013-08-27 18:30:05 +0800

    temp commit

--- src/com/linuo/tongxunlu/ui/DepartmentActivity.java
+++ src/com/linuo/tongxunlu/ui/DepartmentActivity.java
@@ -7,13 +7,17 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.AdapterView;
 import android.widget.AdapterView.OnItemClickListener;
+import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.TextView;
 
 import com.linuo.tongxunlu.R;
 import com.linuo.tongxunlu.adapter.MyListViewAdapter;
 import com.linuo.tongxunlu.data.AppData;
 public class DepartmentActivity extends Activity {
 	private ListView lv;
+	private LinearLayout userinfoLayout,userNameLayout,userPhoneLayout;
+	private TextView userNameText,UserPhoneText;
 //	private 
 
 	@Override
@@ -21,6 +25,9 @@ public class DepartmentActivity extends Activity {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.department);
 		lv = (ListView) findViewById(R.id.melistview);
+		userinfoLayout=(LinearLayout) findViewById(R.id.userinfo);
+		userNameText=(TextView) findViewById(R.id.user_name);
+		UserPhoneText=(TextView) findViewById(R.id.phone_number);
 		lv.setDivider(new ColorDrawable(Color.TRANSPARENT));
 		lv.setDividerHeight(5);
 		lv.setAdapter(new MyListViewAdapter(this, AppData.departmentList));
