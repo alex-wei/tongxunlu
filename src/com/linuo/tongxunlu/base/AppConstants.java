f54e82abe6079dfffeb9b5706ee927dfb1f76e5c
alex-wei <yndgwl@sina.com>
2013-10-21 23:06:31 +0800

    modify fragment

---
+++ src/com/linuo/tongxunlu/base/AppConstants.java
@@ -0,0 +1,11 @@
+package com.linuo.tongxunlu.base;
+
+public class AppConstants {
+	   public static final String TAB_A  = "department";
+	   public static final String TAB_B  = "other_departments";
+	   public static final String TAB_C  = "company";
+	   public static final String TAB_D  = "function";
+
+	   //Your other constants, if you have them..
+
+}
--- src/com/linuo/tongxunlu/ui/MainTabActivity.java
+++ src/com/linuo/tongxunlu/ui/MainTabActivity.java
@@ -1,31 +1,31 @@
 package com.linuo.tongxunlu.ui;
 
-import com.linuo.tongxunlu.R;
-
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
-import android.app.TabActivity;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
 import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.FragmentActivity;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
 import android.widget.TabHost;
-import android.widget.TextView;
 import android.widget.TabHost.TabSpec;
+import android.widget.TextView;
 
-public class MainTabActivity extends TabActivity {
+import com.linuo.tongxunlu.R;
+import com.linuo.tongxunlu.base.AppConstants;
+
+public class MainTabActivity extends FragmentActivity {
 	private TabHost mTabHost;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		setContentView(R.layout.maintab);
 		super.onCreate(savedInstanceState);
-		mTabHost = getTabHost();
-		TabSpec tabMyDepartment = mTabHost.newTabSpec("本部门");
+		mTabHost = (TabHost)findViewById(android.R.id.tabhost);
+		TabSpec tabMyDepartment = mTabHost.newTabSpec(AppConstants.TAB_A);
 		tabMyDepartment
 				.setIndicator(getTabView(R.drawable.tab_main_nav_me, "本部门"));
 		Intent myDepartment = new Intent(this, DepartmentFragmentActivity.class);
@@ -33,21 +33,21 @@ public class MainTabActivity extends TabActivity {
 		tabMyDepartment.setContent(myDepartment);
 		mTabHost.addTab(tabMyDepartment);
 
-		TabSpec tabCompany = mTabHost.newTabSpec("其他部门");
+		TabSpec tabCompany = mTabHost.newTabSpec(AppConstants.TAB_B);
 		tabCompany
 		.setIndicator(getTabView(R.drawable.tab_main_nav_book, "其他部门"));
 		Intent newCompany = new Intent(this, CompanyActivity.class);
 		tabCompany.setContent(newCompany);
 		mTabHost.addTab(tabCompany);
 		
-		TabSpec tabUnit = mTabHost.newTabSpec("全单位");
+		TabSpec tabUnit = mTabHost.newTabSpec(AppConstants.TAB_C);
 		tabUnit
 		.setIndicator(getTabView(R.drawable.tab_main_nav_book, "全单位"));
 		Intent newUnit = new Intent(this, UnitActivity.class);
 		tabUnit.setContent(newUnit);
 		mTabHost.addTab(tabUnit); 
 		
-		TabSpec tabMore = mTabHost.newTabSpec("更多功能");
+		TabSpec tabMore = mTabHost.newTabSpec(AppConstants.TAB_D);
 		tabMore
 		.setIndicator(getTabView(R.drawable.tab_main_nav_book, "更多功能"));
 		Intent newMore = new Intent(this, MoreActivity.class);
@@ -76,6 +76,11 @@ public class MainTabActivity extends TabActivity {
 	public boolean dispatchKeyEvent(KeyEvent event) {
 		if (event.getKeyCode() == KeyEvent.KEYCODE_BACK
 				&& event.getAction() == KeyEvent.ACTION_DOWN) {
+			if (mTabHost.getCurrentTabTag().equals("department")) {
+				
+			} else {
+
+			}
 			AlertDialog.Builder builder = new Builder(this);
 			builder.setTitle("提示");
 			builder.setMessage("确定退出力诺通讯录吗?");
