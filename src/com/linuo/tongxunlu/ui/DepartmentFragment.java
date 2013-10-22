1621e52a36e6ffa988a393da97e57dbbc9fb79ba
alex-wei <yndgwl@sina.com>
2013-10-22 23:00:13 +0800

    use fragment to replace activity

--- src/com/linuo/tongxunlu/ui/DepartmentFragment.java
+++ src/com/linuo/tongxunlu/ui/DepartmentFragment.java
@@ -1,43 +1,45 @@
 package com.linuo.tongxunlu.ui;
 
-import android.app.Activity;
 import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
 import android.os.Bundle;
-import android.support.v4.app.FragmentManager;
-import android.support.v4.app.FragmentTransaction;
-import android.support.v4.app.ListFragment;
+import android.support.v4.app.Fragment;
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemClickListener;
 import android.widget.ListView;
 
 import com.linuo.tongxunlu.R;
 import com.linuo.tongxunlu.adapter.MyListViewAdapter;
+import com.linuo.tongxunlu.base.AppConstants;
 import com.linuo.tongxunlu.data.AppData;
 
-public class DepartmentFragment extends ListFragment {
+public class DepartmentFragment extends Fragment {
+	private ListView lv;
 	
-	@Override
-	public void onActivityCreated(Bundle savedInstanceState) {
-		super.onActivityCreated(savedInstanceState);
-		 Activity activity = getActivity();
-	        
-	        if (activity != null) {
-	        	MyListViewAdapter listAdapter = new MyListViewAdapter(activity, AppData.departmentList);
-	        	setListAdapter(listAdapter);
-	        }
-	}
 	 @Override
-	    public void onListItemClick(ListView l, View v, int position, long id) {
-		 FragmentManager fm = getFragmentManager();
-         
-         if (fm != null) {
-        	 PersonFragment person=new PersonFragment();
-        	 Bundle data=new Bundle();
-        	 data.putInt("position", position);
-        	 person.setArguments(data);
-             FragmentTransaction ft = fm.beginTransaction();
-             ft.replace(R.id.department_container, person);
-             ft.addToBackStack(null);
-             ft.commit();
-         }
-	    }
+	public View onCreateView(LayoutInflater inflater, ViewGroup container,
+			Bundle savedInstanceState) {
+		 View view =inflater.inflate(R.layout.department, container,false);
+		 lv = (ListView) view.findViewById(R.id.melistview);
+			lv.setDivider(new ColorDrawable(Color.TRANSPARENT));
+			lv.setDividerHeight(5);
+			lv.setAdapter(new MyListViewAdapter(getActivity(), AppData.departmentList));
+			lv.setOnItemClickListener(new OnItemClickListener() {
+				public void onItemClick(AdapterView<?> arg0, View arg1, int arg2,
+						long arg3) {
+					// TODO Auto-generated method stub
+					Fragment fragment=new PersonFragment();
+					Bundle data=new Bundle();
+					data.putInt("position", arg2);
+					fragment.setArguments(data);
+					((MainTabActivity)getActivity()).pushFragments(AppConstants.TAB_A, fragment, false, true);
+				}
+				
+			});
+		return view;
+	}
 }
--- src/com/linuo/tongxunlu/ui/MainTabActivity.java
+++ src/com/linuo/tongxunlu/ui/MainTabActivity.java
@@ -1,12 +1,17 @@
 package com.linuo.tongxunlu.ui;
 
+import java.util.HashMap;
+import java.util.Stack;
+
 import android.app.AlertDialog;
 import android.app.AlertDialog.Builder;
 import android.content.DialogInterface;
 import android.content.DialogInterface.OnClickListener;
-import android.content.Intent;
 import android.os.Bundle;
+import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
+import android.support.v4.app.FragmentManager;
+import android.support.v4.app.FragmentTransaction;
 import android.view.KeyEvent;
 import android.view.View;
 import android.widget.ImageView;
@@ -19,42 +24,89 @@ import com.linuo.tongxunlu.base.AppConstants;
 
 public class MainTabActivity extends FragmentActivity {
 	private TabHost mTabHost;
+    private HashMap<String, Stack<Fragment>> mStacks;
+
+    private String mCurrentTab;
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		setContentView(R.layout.maintab);
 		super.onCreate(savedInstanceState);
+		mStacks = new HashMap<String, Stack<Fragment>>();
+	    mStacks.put(AppConstants.TAB_A, new Stack<Fragment>());
+	    mStacks.put(AppConstants.TAB_B, new Stack<Fragment>());
+	    mStacks.put(AppConstants.TAB_C, new Stack<Fragment>());
+	    mStacks.put(AppConstants.TAB_D, new Stack<Fragment>());
 		mTabHost = (TabHost)findViewById(android.R.id.tabhost);
+		mTabHost.setOnTabChangedListener(listener);
+		mTabHost.setup();
 		TabSpec tabMyDepartment = mTabHost.newTabSpec(AppConstants.TAB_A);
+		mTabHost.setCurrentTabByTag(AppConstants.TAB_A);
 		tabMyDepartment
 				.setIndicator(getTabView(R.drawable.tab_main_nav_me, "本部门"));
-		Intent myDepartment = new Intent(this, DepartmentFragmentActivity.class);
-		myDepartment.addFlags(Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY);
-		tabMyDepartment.setContent(myDepartment);
+//		Intent myDepartment = new Intent(this, DepartmentFragmentActivity.class);
+//		myDepartment.addFlags(Intent.FLAG_ACTIVITY_LAUNCHED_FROM_HISTORY);
+		tabMyDepartment.setContent(new TabHost.TabContentFactory() {
+            public View createTabContent(String tag) {
+                return findViewById(R.id.realtabcontent);
+            }
+        });
 		mTabHost.addTab(tabMyDepartment);
 
 		TabSpec tabCompany = mTabHost.newTabSpec(AppConstants.TAB_B);
 		tabCompany
 		.setIndicator(getTabView(R.drawable.tab_main_nav_book, "其他部门"));
-		Intent newCompany = new Intent(this, CompanyActivity.class);
-		tabCompany.setContent(newCompany);
+//		Intent newCompany = new Intent(this, CompanyActivity.class);
+		tabCompany.setContent(new TabHost.TabContentFactory() {
+            public View createTabContent(String tag) {
+                return findViewById(R.id.realtabcontent);
+            }
+        });
 		mTabHost.addTab(tabCompany);
 		
 		TabSpec tabUnit = mTabHost.newTabSpec(AppConstants.TAB_C);
 		tabUnit
 		.setIndicator(getTabView(R.drawable.tab_main_nav_book, "全单位"));
-		Intent newUnit = new Intent(this, UnitActivity.class);
-		tabUnit.setContent(newUnit);
+//		Intent newUnit = new Intent(this, UnitActivity.class);
+		tabUnit.setContent(new TabHost.TabContentFactory() {
+            public View createTabContent(String tag) {
+                return findViewById(R.id.realtabcontent);
+            }
+        });
 		mTabHost.addTab(tabUnit); 
 		
 		TabSpec tabMore = mTabHost.newTabSpec(AppConstants.TAB_D);
 		tabMore
 		.setIndicator(getTabView(R.drawable.tab_main_nav_book, "更多功能"));
-		Intent newMore = new Intent(this, MoreActivity.class);
-		tabMore.setContent(newMore);
+//		Intent newMore = new Intent(this, MoreActivity.class);
+		tabMore.setContent(new TabHost.TabContentFactory() {
+            public View createTabContent(String tag) {
+                return findViewById(R.id.realtabcontent);
+            }
+        });
 		mTabHost.addTab(tabMore);
-
 	}
+	 TabHost.OnTabChangeListener listener = new TabHost.OnTabChangeListener() {
+	      public void onTabChanged(String tabId) {
+	        mCurrentTab = tabId;
+	        if(mStacks.get(tabId).size() == 0){
+	          if(tabId.equals(AppConstants.TAB_A)){
+	            pushFragments(tabId, new DepartmentFragment(), false,true);
+	          }
+	         /* else if(tabId.equals(AppConstants.TAB_B)){
+	            pushFragments(tabId, new DepartmentFragment(), false,true);
+	          }
+	          else if(tabId.equals(AppConstants.TAB_C)){
+	        	pushFragments(tabId, new DepartmentFragment(), false,true);
+	          }
+	          else if(tabId.equals(AppConstants.TAB_D)){
+	    	  pushFragments(tabId, new DepartmentFragment(), false,true);
+	          }*/
+	        }else {
+	          pushFragments(tabId, mStacks.get(tabId).lastElement(), false,false);
+	        }
+	      }
+	    };
 
 	/**
 	 * 返回某一个条目的indicator
@@ -63,6 +115,51 @@ public class MainTabActivity extends FragmentActivity {
 	 * @param text
 	 * @return
 	 */
+	    public void pushFragments(String tag, Fragment fragment,boolean shouldAnimate, boolean shouldAdd){
+	        if(shouldAdd)
+	            mStacks.get(tag).push(fragment);
+	        FragmentManager   manager         =   getSupportFragmentManager();
+	        FragmentTransaction ft            =   manager.beginTransaction();
+	        ft.replace(R.id.realtabcontent, fragment);
+	        ft.commit();
+	      }
+
+
+	      public void popFragments(){
+	        Fragment fragment = mStacks.get(mCurrentTab).elementAt(mStacks.get(mCurrentTab).size() - 2);
+	        mStacks.get(mCurrentTab).pop();
+	        FragmentManager   manager         =   getSupportFragmentManager();
+	        FragmentTransaction ft            =   manager.beginTransaction();
+	        ft.replace(R.id.realtabcontent, fragment);
+	        ft.commit();
+	      }   
+
+
+	  @Override
+	  public void onBackPressed() {
+	 		if(mStacks.get(mCurrentTab).size() == 1){
+	 			AlertDialog.Builder builder = new Builder(this);
+				builder.setTitle("提示");
+				builder.setMessage("确定退出力诺通讯录吗?");
+				builder.setPositiveButton("确定", new OnClickListener() {
+					
+					public void onClick(DialogInterface dialog, int which) {
+						finish();
+					}
+				});
+				builder.setNegativeButton("取消", new OnClickListener() {
+					
+					public void onClick(DialogInterface dialog, int which) {
+						// TODO Auto-generated method stub
+						
+					}
+				});
+				
+				builder.create().show();
+	 		}else{
+	 			popFragments();
+	 		}
+	  	}
 	private View getTabView(int icon, String text) {
 		View view = View.inflate(this, R.layout.tab_main_nav, null);
 		ImageView iv_icon = (ImageView) view.findViewById(R.id.ivIcon);
@@ -72,15 +169,10 @@ public class MainTabActivity extends FragmentActivity {
 		return view;
 	}
 
-	@Override
+	/*@Override
 	public boolean dispatchKeyEvent(KeyEvent event) {
 		if (event.getKeyCode() == KeyEvent.KEYCODE_BACK
 				&& event.getAction() == KeyEvent.ACTION_DOWN) {
-			if (mTabHost.getCurrentTabTag().equals("department")) {
-				
-			} else {
-
-			}
 			AlertDialog.Builder builder = new Builder(this);
 			builder.setTitle("提示");
 			builder.setMessage("确定退出力诺通讯录吗?");
@@ -103,5 +195,5 @@ public class MainTabActivity extends FragmentActivity {
 		}
 
 		return super.dispatchKeyEvent(event);
-	}
+	}*/
 }
